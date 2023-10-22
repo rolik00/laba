735dbb8da180e902881c3c3c51da02c7e23b801f
rolik00 <148611487+rolik00@users.noreply.github.com>
2023-10-22 11:59:00 +0300

    Create second_way.cpp

---
+++ second_way.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+int table[10000][10000];
+
+int main()
+{
+    int n = 8192, m, target = 2 * n - 1;
+    for (int k = 1; k <= 13; k++)
+    {
+        m = pow(2, k);
+        std::cout << m << "\n";
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++)
+            table[i][j] = ((n / m) * i + j + 1) * 2;
+        int i_2 = 0, j_2 = m - 1, f_2 = 0;
+        while (i_2 >= 0 && i_2 < n && j_2 >= 0 && j_2 < m)
+        {
+            if (table[i_2][j_2] == target)
+            {
+                f_2 = 1;
+                std::cout << "YES" << std::endl;
+                break;
+            }
+            else if (table[i_2][j_2] < target) i_2++;
+            else j_2--;
+        }
+        if (f_2 == 0) std::cout << "NO" << std::endl;
+    }
+    return 0;
+}
